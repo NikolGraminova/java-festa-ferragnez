159dc7f5dfbcad80ea3bff227b405607732bcd8b
NikolGraminova <nbackup619@gmail.com>
2023-07-13 14:48:30 +0200

    Finished exercise

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,7 +1,34 @@
 package com.ferragnez.party;
 
+/*
+Nel programma bisogna:
+creare e inizializzare l’array contenente i nomi degli invitati
+chiedere all’utente come si chiama
+verificare che il nome sia presente nella lista lasciarlo entrare o rispedirlo cortesemente da dove è venuto
+Bonus: se avete usato il ciclo for per cercare il nome nella lista nella consegna base, implementare lo stesso programma usando il ciclo while, e viceversa
+ */
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class CheckGuest {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] guests = {"Dua Lipa","Paris Hilton","Manuel Agnelli","Francesco Totti", "Ilary Blasi","Bebe Vio","Luis","Paradis Zarei","Martina Maccherone","Rachel Zeilic"};
 
+        boolean check = false;
+
+        while (!check){
+            System.out.println("Enter your name: ");
+            String user = scan.nextLine();
+            if (Arrays.asList(guests).contains(user)) {
+                System.out.println("You can get in.");
+                check = true;
+            } else {
+                System.out.println("Get out.");
+            }
+        }
+
+        scan.close();
     }
 }
